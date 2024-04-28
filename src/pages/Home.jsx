24e281d1570fef6d5ea9c5c929d2dbe1aa3a6648
fayshal69 <foyshalbinamir@gmail.com>
2024-04-28 22:50:10 +0600

    updated result deiv

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,25 +17,29 @@ const Home = () => {
             return alert("Please fill the input box");
         }
         const { result, similarLineOutput } = handleResult(text1, text2);
-        setResult(result);
+        setResult(result); 
         setLines(similarLineOutput);
     }
 
     return (
         <div className="container mx-auto min-h-screen flex flex-col justify-center items-center px-5">
+            <h1 className="text-3xl md:text-5xl">plagiarism-checker</h1>
             <form onSubmit={handleInput} className="w-full md:w-1/2 border p-4 mb-8">
                 <textarea name="text1" id="" rows="5" placeholder="Type here... " className="border-2 rounded border-teal-500 w-full p-4 mb-4"></textarea>
                 <textarea name="text2" id="" rows="5" placeholder="Type here... " className="border-2 rounded border-teal-500 w-full p-4 mb-8"></textarea>
                 <button className="w-full bg-green-600 text-white text-lg font-semibold py-2 rounded">Check</button>
             </form>
-            <div className="text-left w-full md:w-1/2 space-y-6">
-                <p className="text-2xl font-medium">Similarity : <span className="text-red-500">{result} %</span></p>
-                <div className="flex items-center gap-4">
-                    {
-                        lines.map((line, i) => <p key={i} className=" bg-amber-200 px-2 py-1 rounded">{line}</p>)
-                    }
+            {
+               result !== '' &&
+                <div className="text-left w-full md:w-1/2 space-y-6">
+                    <p className="text-2xl font-medium">Similarity : <span className="text-red-500">{result} %</span></p>
+                    <div className="flex items-center gap-4">
+                        {
+                            lines.map((line, i) => <p key={i} className=" bg-amber-200 px-2 py-1 rounded">{line}</p>)
+                        }
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     );
 };
